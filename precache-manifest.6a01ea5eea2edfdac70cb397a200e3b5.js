05c5d1423770096639ff296e57c07863dbc5ff72
mohanedkohil11 <moha.kohil@gmail.com>
2020-08-25 13:00:06 +0200

    Updates

---
+++ precache-manifest.6a01ea5eea2edfdac70cb397a200e3b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8360cb832d994a44b7e70caaec9c0b7",
+    "url": "/Notes-theme2/index.html"
+  },
+  {
+    "revision": "ec255c97a4334c363244",
+    "url": "/Notes-theme2/static/css/2.c1a69a2b.chunk.css"
+  },
+  {
+    "revision": "a04d3208fb24ce8ac5d1",
+    "url": "/Notes-theme2/static/css/main.fd3eb334.chunk.css"
+  },
+  {
+    "revision": "ec255c97a4334c363244",
+    "url": "/Notes-theme2/static/js/2.d87fe485.chunk.js"
+  },
+  {
+    "revision": "6e006f6675cbe65c76c1c344aaa56010",
+    "url": "/Notes-theme2/static/js/2.d87fe485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04d3208fb24ce8ac5d1",
+    "url": "/Notes-theme2/static/js/main.f9768272.chunk.js"
+  },
+  {
+    "revision": "8e36db5753f20245ea43",
+    "url": "/Notes-theme2/static/js/runtime-main.adf4fb9e.js"
+  },
+  {
+    "revision": "971a78e4bb738674f283eb3b2ac646c2",
+    "url": "/Notes-theme2/static/media/logo.971a78e4.ico"
+  }
+]);
